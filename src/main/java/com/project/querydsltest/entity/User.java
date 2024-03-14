b2d92b964d8e03e300dd231f56dea3775872ad60
yiyaaa <thdtjs1219@gmail.com>
2024-03-14 12:16:04 +0900

    feat: entity setting

---
+++ src/main/java/com/project/querydsltest/entity/User.java
@@ -0,0 +1,22 @@
+package com.project.querydsltest.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
---
+++ src/main/java/com/project/querydsltest/entity/WebtoonLike.java
@@ -0,0 +1,22 @@
+package com.project.querydsltest.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class WebtoonLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "webtoon_id", nullable = false)
+    private Long webtoonId;
+}
